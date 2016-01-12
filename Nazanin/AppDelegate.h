39ecf89d2af81d722ddaa5a36294e98422165123
Abdul Qavi <aqavi@abduls-macbook-air.local>
2016-01-12 08:07:04 +0500

    Initial Commit

---
+++ Nazanin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nazanin
+//
+//  Created by Abdul Qavi on 12/01/2016.
+//  Copyright © 2016 NikaLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nazanin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nazanin
+//
+//  Created by Abdul Qavi on 12/01/2016.
+//  Copyright © 2016 NikaLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
